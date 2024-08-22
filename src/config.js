993b019749d7cd7a89e321c113384ddb57295549
Turya Biswas <78296735+turyabiswas233@users.noreply.github.com>
2024-08-22 14:35:31 +0600

    updated PK

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 const config = {
   API_BASE_URL: 'https://urlocker-api.onrender.com',
   GOOGLE_API_KEY: 'AIzaSyDyWau177aZz12QPehbOqhi8MCDfNIjN3I',
-  STRIPE_PUBLIC_KEY: 'pk_test_51PbxMUBwcWogP8JAxnI1t2C7jayxvKeqbRGbr0QhSGQFAfkUBk3iy22q9e1Mdufffi4lQvzn7uoowbzcfmw8dPgL001pJhDXzU',
+  // STRIPE_PUBLIC_KEY: 'pk_test_51PbxMUBwcWogP8JAxnI1t2C7jayxvKeqbRGbr0QhSGQFAfkUBk3iy22q9e1Mdufffi4lQvzn7uoowbzcfmw8dPgL001pJhDXzU',
+  STRIPE_PUBLIC_KEY: 'pk_live_51PbxMUBwcWogP8JA1YhMcYc9tiSrLV04XsjkS5dEQZsKhdst7Y6KaVK8SmsvK4rVAXouklr8UTQl3YlYeqtNu98T00PVhgg5jk',
 };
 
 export default config;
