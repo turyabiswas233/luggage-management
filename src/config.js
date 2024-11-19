ee3a65cd0850633be21ed66727527f10720323fc
turyabiswas233 <mydelllaptop233@gmail.com>
2024-11-19 21:06:50 +0600

    switch to test

--- src/config.js
+++ src/config.js
@@ -7,9 +7,9 @@ const config = {
   GOOGLE_API_KEY: 'AIzaSyDyWau177aZz12QPehbOqhi8MCDfNIjN3I',
   // urloker stripe
   // STRIPE_PUBLIC_KEY: 'pk_test_51PbxMUBwcWogP8JAxnI1t2C7jayxvKeqbRGbr0QhSGQFAfkUBk3iy22q9e1Mdufffi4lQvzn7uoowbzcfmw8dPgL001pJhDXzU',
-  STRIPE_PUBLIC_KEY: 'pk_live_51PbxMUBwcWogP8JA1YhMcYc9tiSrLV04XsjkS5dEQZsKhdst7Y6KaVK8SmsvK4rVAXouklr8UTQl3YlYeqtNu98T00PVhgg5jk',
+  // STRIPE_PUBLIC_KEY: 'pk_live_51PbxMUBwcWogP8JA1YhMcYc9tiSrLV04XsjkS5dEQZsKhdst7Y6KaVK8SmsvK4rVAXouklr8UTQl3YlYeqtNu98T00PVhgg5jk',
   // my stripe
-  // STRIPE_PUBLIC_KEY: 'pk_test_51PoqIcIHm7C3ICY9JcYtNRObu9zhF4SkA8eaRza3GIv0jPbHcKCReQpXaMquCSDVRpDouRInUyboGA76zKW8XdFA00vNkUrgnv',
+  STRIPE_PUBLIC_KEY: 'pk_test_51PoqIcIHm7C3ICY9JcYtNRObu9zhF4SkA8eaRza3GIv0jPbHcKCReQpXaMquCSDVRpDouRInUyboGA76zKW8XdFA00vNkUrgnv',
 };
 
 export default config;
